2e02f3f013bf1d7fa525785b745c13271c1b2257
manugcr <mgilcernich@gmail.com>
2024-03-12 17:02:52 -0300

    added new test

---
+++ proyectobase/input/input2.c
@@ -0,0 +1,17 @@
+int addition(int, int);
+
+int main() {
+    int var1 = 3;
+    int var2;
+
+    var2 = 4*5;
+
+    int result = addition(var1, var2);
+    result++;
+
+    return 0;    
+}
+
+int addition(int a, int b) {
+    return a + b;
+}
